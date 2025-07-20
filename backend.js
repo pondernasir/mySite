618f7e4c8dc0cb694f078ef74cd5bec5453e3614
Naz <pondernasir.personal@gmail.com>
2025-07-19 23:15:27 -0400

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,16 @@
+function x (){
+    const name = document.getElementById('name');
+    name.classList.toggle('x');
+
+    const links = document.getElementById('links');
+    links.classList.toggle('x');
+
+    const menuBtn = document.getElementById('menuBtn');
+    menuBtn.classList.toggle('x');
+
+    const footer = document.getElementById('footer');
+    footer.classList.toggle('x');
+
+    const body = document.getElementById('body');
+    body.classList.toggle('x');
+}
